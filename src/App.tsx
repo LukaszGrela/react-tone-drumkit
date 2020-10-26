77b00acde1be5fbc24cc158f9bdd51360fb2d15f
Lukasz 'Severiaan' Grela <lukasz.grela@gmail.com>
2020-10-26 22:38:15 +0100

    Add initiated state handle keyboard clicks Tap to start button

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,55 @@
 import React from 'react';
-import { Players } from 'tone';
+import { Players, start as toneStart } from 'tone';
 import './App.css';
 import Key from './Key/Key';
 
 const instruments = [
-  /* A */ { keyCode: 65, label: 'clap' },
-  /* S */ { keyCode: 83, label: 'hihat' },
-  /* D */ { keyCode: 68, label: 'kick' },
-  /* F */ { keyCode: 70, label: 'openhat' },
-  /* G */ { keyCode: 71, label: 'boom' },
-  /* H */ { keyCode: 72, label: 'ride' },
-  /* J */ { keyCode: 74, label: 'snare' },
-  /* K */ { keyCode: 75, label: 'tom' },
-  /* L */ { keyCode: 76, label: 'tink' },
+  { key: 'A', keyCode: 65, label: 'clap' },
+  { key: 'S', keyCode: 83, label: 'hihat' },
+  { key: 'D', keyCode: 68, label: 'kick' },
+  { key: 'F', keyCode: 70, label: 'openhat' },
+  { key: 'G', keyCode: 71, label: 'boom' },
+  { key: 'H', keyCode: 72, label: 'ride' },
+  { key: 'J', keyCode: 74, label: 'snare' },
+  { key: 'K', keyCode: 75, label: 'tom' },
+  { key: 'L', keyCode: 76, label: 'tink' },
 ];
 
 const App: React.FC = (): JSX.Element => {
+  const [initiated, setInitiated] = React.useState(false);
   const [loading, setLoading] = React.useState(true);
   const players = new Players(
     instruments.reduce((acc, instrument) => {
       return {
         ...acc,
-        [String.fromCharCode(
-          instrument.keyCode
-        )]: `./sound/${instrument.label}.mp3`,
+        [instrument.key]: `./sound/${instrument.label}.mp3`,
       };
     }, {}),
     (): void => {
       setLoading(false);
-      players.toDestination()
+      players.toDestination();
     }
   );
+  const downHandler = (e: KeyboardEvent): void => {
+    const instrument = instruments.find(
+      (needle): boolean => needle.keyCode === e.which
+    );
+    if (instrument) {
+      if (players.has(instrument.key)) {
+        players.player(instrument.key).start();
+      }
+    }
+  };
+  // const upHandler = (e: KeyboardEvent): void => {};
+  React.useEffect((): (() => void) => {
+    window.addEventListener('keydown', downHandler);
+    // window.addEventListener('keyup', upHandler);
+    return (): void => {
+      window.removeEventListener('keydown', downHandler);
+      // window.removeEventListener('keyup', upHandler);
+    };
+  }, []);
+
   return (
     <div className='App'>
       <header className='App-header'>
@@ -41,23 +60,32 @@ const App: React.FC = (): JSX.Element => {
         <div className='keys'>
           {loading && <p>LOADING...</p>}
           {!loading &&
+            initiated &&
             instruments.map(
-              (instrument): JSX.Element => (
+              ({ key, keyCode, label }): JSX.Element => (
                 <Key
-                  key={instrument.keyCode}
-                  keyCode={instrument.keyCode}
-                  label={instrument.label}
+                  key={keyCode}
+                  keyCode={keyCode}
+                  label={label}
                   onClick={(): void => {
-                    const sound = String.fromCharCode(
-                      instrument.keyCode
-                    );
-                    if(players.has(sound)) {
-                      players.player(sound).start()
+                    if (players.has(key)) {
+                      players.player(key).start();
                     }
                   }}
                 />
               )
             )}
+          {!loading && !initiated && (
+            <button
+              className='tap-to-start'
+              onClick={async () => {
+                await toneStart();
+                setInitiated(true);
+              }}
+            >
+              Tap to start
+            </button>
+          )}
         </div>
       </section>
     </div>
