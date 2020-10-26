c1b3105aa3bbcd4cfb04b4909a807844b4e6be51
Lukasz 'Severiaan' Grela <lukasz.grela@gmail.com>
2020-10-26 21:27:09 +0100

    Define instruments, play them on click

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,63 @@
 import React from 'react';
+import { Players } from 'tone';
 import './App.css';
 import Key from './Key/Key';
 
+const instruments = [
+  /* A */ { keyCode: 65, label: 'clap' },
+  /* S */ { keyCode: 83, label: 'hihat' },
+  /* D */ { keyCode: 68, label: 'kick' },
+  /* F */ { keyCode: 70, label: 'openhat' },
+  /* G */ { keyCode: 71, label: 'boom' },
+  /* H */ { keyCode: 72, label: 'ride' },
+  /* J */ { keyCode: 74, label: 'snare' },
+  /* K */ { keyCode: 75, label: 'tom' },
+  /* L */ { keyCode: 76, label: 'tink' },
+];
+
 const App: React.FC = (): JSX.Element => {
+  const [loading, setLoading] = React.useState(true);
+  const players = new Players(
+    instruments.reduce((acc, instrument) => {
+      return {
+        ...acc,
+        [String.fromCharCode(
+          instrument.keyCode
+        )]: `./sound/${instrument.label}.mp3`,
+      };
+    }, {}),
+    (): void => {
+      setLoading(false);
+      players.toDestination()
+    }
+  );
   return (
     <div className='App'>
       <header className='App-header'>
         <p>Drum Kit</p>
       </header>
+
       <section className='App-section'>
         <div className='keys'>
-          {/* A */}
-          <Key keyCode={65} label={'clap'}></Key>
-          {/* S */}
-          <Key keyCode={83} label={'hihat'}></Key>
-          {/* D */}
-          <Key keyCode={68} label={'kick'}></Key>
-          {/* F */}
-          <Key keyCode={70} label={'openhat'}></Key>
-          {/* G */}
-          <Key keyCode={71} label={'boom'}></Key>
-          {/* H */}
-          <Key keyCode={72} label={'ride'}></Key>
-          {/* J */}
-          <Key keyCode={74} label={'snare'}></Key>
-          {/* K */}
-          <Key keyCode={75} label={'tom'}></Key>
-          {/* L */}
-          <Key keyCode={76} label={'tink'}></Key>
+          {loading && <p>LOADING...</p>}
+          {!loading &&
+            instruments.map(
+              (instrument): JSX.Element => (
+                <Key
+                  key={instrument.keyCode}
+                  keyCode={instrument.keyCode}
+                  label={instrument.label}
+                  onClick={(): void => {
+                    const sound = String.fromCharCode(
+                      instrument.keyCode
+                    );
+                    if(players.has(sound)) {
+                      players.player(sound).start()
+                    }
+                  }}
+                />
+              )
+            )}
         </div>
       </section>
     </div>
