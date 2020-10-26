25b13a0102be5c967d2748654befaee634e6c82a
Lukasz 'Severiaan' Grela <lukasz.grela@gmail.com>
2020-10-26 21:26:04 +0100

    onClick handler

--- src/Key/Key.tsx
+++ src/Key/Key.tsx
@@ -3,11 +3,12 @@ import React from 'react';
 export interface IProps {
   keyCode: number;
   label: string;
+  onClick: () => void;
 }
 
 const Key: React.FC<IProps> = (props: IProps): JSX.Element => {
   return (
-    <div className="key">
+    <div className='key' onClick={props.onClick}>
       <kbd>{String.fromCharCode(props.keyCode)}</kbd>
       <span className='label'>{props.label}</span>
     </div>
