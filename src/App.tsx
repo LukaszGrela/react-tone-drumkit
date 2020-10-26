fdc20af29c25cd4b580ae14637da65ac1b825fa0
Lukasz 'Severiaan' Grela <lukasz.grela@gmail.com>
2020-10-26 08:30:42 +0100

    Add Key's for each instrument

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Key from './Key/Key';
 
-function App() {
+const App: React.FC = (): JSX.Element => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <p>Drum Kit</p>
       </header>
+      <section className='App-section'>
+        <div className='keys'>
+          {/* A */}
+          <Key keyCode={65} label={'clap'}></Key>
+          {/* S */}
+          <Key keyCode={83} label={'hihat'}></Key>
+          {/* D */}
+          <Key keyCode={68} label={'kick'}></Key>
+          {/* F */}
+          <Key keyCode={70} label={'openhat'}></Key>
+          {/* G */}
+          <Key keyCode={71} label={'boom'}></Key>
+          {/* H */}
+          <Key keyCode={72} label={'ride'}></Key>
+          {/* J */}
+          <Key keyCode={74} label={'snare'}></Key>
+          {/* K */}
+          <Key keyCode={75} label={'tom'}></Key>
+          {/* L */}
+          <Key keyCode={76} label={'tink'}></Key>
+        </div>
+      </section>
     </div>
   );
-}
+};
 
 export default App;
